2b87156f31a4a978cd352b81b495e26760f9b08a
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-03-04 17:30:08 +0330

    implement db connection and migrations as well

---
+++ store/db.go
@@ -0,0 +1,25 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/saleh-ghazimoradi/Async-api-in-Golang/config"
+	"time"
+)
+
+func NewPostgresDB(conf *config.Config) (*sql.DB, error) {
+	dsn := conf.DatabaseURL()
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+	return db, nil
+}
